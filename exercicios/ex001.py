d9af82a12c8f3ae8c668cbce62264acf1d8aaf90
Alan Solima <alancontato03@gmail.com>
2024-07-27 14:15:14 -0300

    Add files via upload

---
+++ exercicios/ex001.py
@@ -0,0 +1,7 @@
+# Exercício 001: Olá, Mundo!
+
+# Objetivo: Aprender a usar a função `print` para mostrar uma mensagem na tela.
+
+# A função `print` exibe a mensagem que está entre aspas na tela do usuário.
+# Neste caso, estamos mostrando a mensagem 'Olá, Mundo!'.
+print('Olá, Mundo!')  # Exibe a mensagem 'Olá, Mundo!' na tela.
